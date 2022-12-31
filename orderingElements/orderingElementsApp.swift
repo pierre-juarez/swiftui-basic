14d773e2d5913cda30f0de423b8b01d57fb22508
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-12-30 23:20:15 -0500

    Initial commit

---
+++ orderingElements/orderingElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  orderingElementsApp.swift
+//  orderingElements
+//
+//  Created by Pierre Juarez U. on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct orderingElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
